d2d6de5f40041871d553b9ae6b41714a46147e01
Mohamed Ali Addi <m.a.addi@uu.nl>
2020-01-15 16:51:12 +0100

    idc

--- train.py
+++ train.py
@@ -11,8 +11,8 @@ import sys
 os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
 start_epoch = 1
 #save_dir = os.path.join(save_dir, datetime.now().strftime('%Y%m%d_%H%M%S'))
-if os.path.exists(save_dir):
-    raise NameError('model dir exists!')
+#if os.path.exists(save_dir):
+#    raise NameError('model dir exists!')
 os.makedirs(save_dir)
 logging = init_log(save_dir)
 _print = logging.info
@@ -150,6 +150,7 @@ for epoch in range(start_epoch, 1):
         net_state_dict = net.module.state_dict()
         if not os.path.exists(save_dir):
             os.mkdir(save_dir)
+        print("saving model")
         torch.save({
             'epoch': epoch,
             'train_loss': train_loss,
@@ -158,5 +159,5 @@ for epoch in range(start_epoch, 1):
             'test_acc': test_acc,
             'net_state_dict': net_state_dict},
             os.path.join('/gdrive/My Drive/mpr/models/', 'model.ckpt'))
-
+        print("saved model")
 print('finishing training')
