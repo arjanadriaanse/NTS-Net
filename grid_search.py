a9e023c3c6f8afce06c26108425ce09f97f48646
Mohamed Ali Addi <m.a.addi@uu.nl>
2020-01-18 21:04:47 +0100

    clearing of all accuracies textfile

--- grid_search.py
+++ grid_search.py
@@ -2,7 +2,7 @@ import os
 from config import accuracy_text_location
 small_sizes_grid = [36,48, 60]
 medium_sizes_grid = [84,96, 108]
-large_sizes_grid = [180,192,104]
+large_sizes_grid = [180,192,204]
 
 best_accuracy = 0.0 #The initial value
 best_small  = -1
@@ -25,10 +25,11 @@ def write_accuracy_for_config(small, medium, large, accuracy):
     
 
 #Call train with all possible combinations of anchors
+open(os.path.join(accuracy_text_location, "all_accuracies.txt"), 'w').close()
 for small_scale in small_sizes_grid:
 	for medium_scale in medium_sizes_grid:
 		for large_scale in large_sizes_grid:
-			print("training" + str(small_scale) + ' ' + str(medium_scale) + ' ' + str(large_scale))
+			print("training : " + str(small_scale) + ' ' + str(medium_scale) + ' ' + str(large_scale))
 			#Training for current config
 			os.system('python3 train.py ' + str(small_scale) + ' ' + str(medium_scale) + ' ' + str(large_scale))
 			print("done training")
