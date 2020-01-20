43ab99e7e15652037658010de78b2962d4065bc1
Mark <markheinsbroek@gmail.com>
2020-01-20 17:03:30 +0100

    loss test

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ if resume:
     net.load_state_dict(ckpt['net_state_dict'])
     start_epoch = ckpt['epoch'] + 1
 #creterion = loss.CrossEntropyLoss()
-creterion = torch.nn.NLLLoss()
+creterion = torch.nn.CrossEntropyLoss()
 
 # define optimizers
 raw_parameters = list(net.pretrained_model.parameters())
@@ -67,13 +67,13 @@ for epoch in range(start_epoch, 500):
         raw_logits, concat_logits, part_logits, _, top_n_prob = net(img)
         part_loss = model.list_loss(part_logits.view(batch_size * PROPOSAL_NUM, -1),
                                     label.unsqueeze(1).repeat(1, PROPOSAL_NUM).view(-1)).view(batch_size, PROPOSAL_NUM)
-        raw_loss = creterion(raw_logits, label)
+        #raw_loss = creterion(raw_logits, label)
         concat_loss = creterion(concat_logits, label)
         rank_loss = model.ranking_loss(top_n_prob, part_loss)
         partcls_loss = creterion(part_logits.view(batch_size * PROPOSAL_NUM, -1),
                                  label.unsqueeze(1).repeat(1, PROPOSAL_NUM).view(-1))
 
-        total_loss = raw_loss + rank_loss + concat_loss + partcls_loss
+        total_loss = """raw_loss +""" rank_loss + concat_loss + partcls_loss
         total_loss.backward()
         raw_optimizer.step()
         part_optimizer.step()
