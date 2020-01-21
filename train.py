05df0ee9c3ca4419596b0c3f17b40a1cd7b4cdf3
Mark <markheinsbroek@gmail.com>
2020-01-21 11:26:59 +0100

    experiment 3

--- train.py
+++ train.py
@@ -29,8 +29,9 @@ if resume:
     ckpt = torch.load(resume)
     net.load_state_dict(ckpt['net_state_dict'])
     start_epoch = ckpt['epoch'] + 1
-creterion = loss.CrossEntropyLoss()
-#creterion = torch.nn.CrossEntropyLoss()
+#creterion = loss.CrossEntropyLoss()
+creterion = torch.nn.CrossEntropyLoss()
+creterion2 = torch.nn.softmax_cross_entropy_with_logits()
 
 # define optimizers
 raw_parameters = list(net.pretrained_model.parameters())
@@ -67,8 +68,8 @@ for epoch in range(start_epoch, 500):
         raw_logits, concat_logits, part_logits, _, top_n_prob = net(img)
         part_loss = model.list_loss(part_logits.view(batch_size * PROPOSAL_NUM, -1),
                                     label.unsqueeze(1).repeat(1, PROPOSAL_NUM).view(-1)).view(batch_size, PROPOSAL_NUM)
-        raw_loss = creterion(raw_logits, label)
-        concat_loss = creterion(concat_logits, label)
+        raw_loss = creterion2(raw_logits, label)
+        concat_loss = creterion2(concat_logits, label)
         rank_loss = model.ranking_loss(top_n_prob, part_loss)
         partcls_loss = creterion(part_logits.view(batch_size * PROPOSAL_NUM, -1),
                                  label.unsqueeze(1).repeat(1, PROPOSAL_NUM).view(-1))
