ade2b6736d2fc0ab8bd56a8cf49e3b5a105700f1
Mark <markheinsbroek@gmail.com>
2020-01-21 13:00:50 +0100

    experiment 5c

--- train.py
+++ train.py
@@ -31,7 +31,9 @@ if resume:
     start_epoch = ckpt['epoch'] + 1
 
 #creterion = loss.CrossEntropyLoss()
-creterion = torch.nn.BCEWithLogitsLoss()
+creterion = torch.nn.BCELoss()
+creterion2 = torch.nn.CrossEntropyLoss()
+m = torch.nn.Sigmoid()
 
 # define optimizers
 raw_parameters = list(net.pretrained_model.parameters())
@@ -65,13 +67,14 @@ for epoch in range(start_epoch, 500):
         concat_optimizer.zero_grad()
         partcls_optimizer.zero_grad()
 
+
         raw_logits, concat_logits, part_logits, _, top_n_prob = net(img)
         part_loss = model.list_loss(part_logits.view(batch_size * PROPOSAL_NUM, -1),
                                     label.unsqueeze(1).repeat(1, PROPOSAL_NUM).view(-1)).view(batch_size, PROPOSAL_NUM)
-        raw_loss = creterion(raw_logits, label)
-        concat_loss = creterion(concat_logits, label)
+        raw_loss = creterion(m(raw_logits), label)
+        concat_loss = creterion(m(concat_logits), label)
         rank_loss = model.ranking_loss(top_n_prob, part_loss)
-        partcls_loss = creterion(part_logits.view(batch_size * PROPOSAL_NUM, -1),
+        partcls_loss = creterion2(part_logits.view(batch_size * PROPOSAL_NUM, -1),
                                  label.unsqueeze(1).repeat(1, PROPOSAL_NUM).view(-1))
         
         #The model increases in accuracy less rapidly without the normalisation.
